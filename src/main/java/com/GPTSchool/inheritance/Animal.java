7fbe5cb46439f0c31fa9874b09d316d58aef13e2
Sami Aibeche <samiaibeche@gmail.com>
2023-11-18 17:44:07 +0100

    Dive deeper into OOP (Inheritance, polymorphism, etc.). update

---
+++ src/main/java/com/GPTSchool/inheritance/Animal.java
@@ -0,0 +1,7 @@
+package com.GPTSchool.inheritance;
+
+public class Animal {
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+}
---
+++ src/main/java/com/GPTSchool/interfaces/Dog.java
@@ -0,0 +1,8 @@
+package com.GPTSchool.interfaces;
+
+public class Dog implements Animal {
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+}
